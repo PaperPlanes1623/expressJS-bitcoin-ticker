85b7e5c62ee577e1c8d944d8336bcbaa2e7535ee
Dylan Taft <dylbronjames@gmail.com>
2019-09-11 19:55:23 -0600

    added quantity input

--- index.js
+++ index.js
@@ -16,17 +16,30 @@ app.post("/", function (req, res) {
   var crypto = req.body.crypto;
   var fiat = req.body.fiat;
 
-  var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-  var finalURL = baseURL + crypto + fiat;
-  request(finalURL, function (error, response, body) {
-    var data = JSON.parse(body);
-    var price = data.last;
+  var amount = req.body.amount;
 
-    var currentDate = data.display_timestamp;
+  var baseURL = "https://apiv2.bitcoinaverage.com/convert/global";
+
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  }
+
+  request(options, function (error, response, body) {
+    var data = JSON.parse(body);
+    var price = data.price;
+    console.log(price);
+
+    var currentDate = data.time;
 
     res.write("<p>The current date is " + currentDate + "</p>");
 
-    res.write("<h1>The Current price of " + crypto + " is " + price + fiat + "</h1>");
+    res.write("<h1>" + amount + " " + crypto + " is currently worth " + price + " " + fiat + "</h1>");
 
     res.send();
   })
